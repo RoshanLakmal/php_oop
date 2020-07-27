5e57ff3ff8b36af76e77ec260184fb6a6018f0b4
Roshan Elvitigala <roshanrox911@gmail.com>
2020-07-27 12:26:20 +1000

    sending file html added

---
+++ upload.php
@@ -0,0 +1,16 @@
+<?php 
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+<form action="upload.php" enctype="multipart/form-data" method="post">
+	<input type="file" name="file_upload"><br>
+	<input type="submit" name="submit">
+</form>
+</body>
+</html>
